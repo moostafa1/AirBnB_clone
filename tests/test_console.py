bcb8d6e827290e6d15a2a80b8d62a7847b94bd76
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-12-09 15:31:06 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,7 +17,7 @@ class TestConsole(unittest.TestCase):
         sys.stdout = self.cap
 
     def tearDown(self):
-        ''' tearDown function '''
+        ''' TearDown function '''
         sys.stdout = self.copy
 
     def create(self):
@@ -26,89 +26,89 @@ class TestConsole(unittest.TestCase):
 
     def test_quit_exists(self):
         ''' Testing quit exists '''
-        consl = self.create()
-        self.assertTrue(consl.onecmd("quit"))
+        cnsl = self.create()
+        self.assertTrue(cnsl.onecmd("quit"))
 
     def test_EOF_exists(self):
         ''' Testing EOF exists '''
-        consl = self.create()
-        self.assertTrue(consl.onecmd("EOF"))
+        cnsl = self.create()
+        self.assertTrue(cnsl.onecmd("EOF"))
 
     def test_all_method(self):
         ''' Testing all method '''
-        consl = self.create()
-        consl.onecmd("all")
+        cnsl = self.create()
+        cnsl.onecmd("all")
         self.assertTrue(isinstance(self.cap.getvalue(), str))
 
     def test_show_method(self):
         ''' Testing show method '''
-        consl = self.create()
-        consl.onecmd("create User")
+        cnsl = self.create()
+        cnsl.onecmd("create User")
         user_id = self.cap.getvalue()
         sys.stdout = self.copy
         self.cap.close()
         self.cap = StringIO()
         sys.stdout = self.cap
-        consl.onecmd("show User " + user_id)
+        cnsl.onecmd("show User " + user_id)
         output = (self.cap.getvalue())
         sys.stdout = self.copy
         self.assertTrue(str is type(output))
 
     def test_display_class_name(self):
         ''' Test: display error message if class is missing '''
-        consl = self.create()
-        consl.onecmd("create User")
+        cnsl = self.create()
+        cnsl.onecmd("create User")
         sys.stdout = self.copy
         self.cap.close()
         self.cap = StringIO()
         sys.stdout = self.cap
-        consl.onecmd("show")
+        cnsl.onecmd("show")
         output = (self.cap.getvalue())
         sys.stdout = self.copy
         self.assertEqual("** class name missing **\n", output)
 
     def test_class_id(self):
         ''' Test: display error message if id is missing '''
-        consl = self.create()
-        consl.onecmd("create User")
+        cnsl = self.create()
+        cnsl.onecmd("create User")
         sys.stdout = self.copy
         self.cap.close()
         self.cap = StringIO()
         sys.stdout = self.cap
-        consl.onecmd("show User")
+        cnsl.onecmd("show User")
         output = (self.cap.getvalue())
         sys.stdout = self.copy
         self.assertEqual("** instance id missing **\n", output)
 
     def test_display_instance_not_found(self):
         ''' Test: display error message if id is missing '''
-        consl = self.create()
-        consl.onecmd("create User")
+        cnsl = self.create()
+        cnsl.onecmd("create User")
         sys.stdout = self.copy
         self.cap.close()
         self.cap = StringIO()
         sys.stdout = self.cap
-        consl.onecmd("show User " + "124356876")
+        cnsl.onecmd("show User " + "124356876")
         output = (self.cap.getvalue())
         sys.stdout = self.copy
         self.assertEqual("** no instance found **\n", output)
 
     def test_create_method(self):
         ''' Test: create method '''
-        consl = self.create()
-        consl.onecmd("create User")
+        cnsl = self.create()
+        cnsl.onecmd("create User")
         self.assertTrue(isinstance(self.cap.getvalue(), str))
 
     def test_class_name(self):
         ''' Checks error message if class name doesn't exist '''
-        consl = self.create()
-        consl.onecmd("create")
+        cnsl = self.create()
+        cnsl.onecmd("create")
         output = (self.cap.getvalue())
         self.assertEqual("** class name missing **\n", output)
 
     def test_class_name_missing(self):
         ''' Checks error message if class name doesn't exist '''
-        consl = self.create()
-        consl.onecmd("create Something")
+        cnsl = self.create()
+        cnsl.onecmd("create Something")
         output = (self.cap.getvalue())
         self.assertEqual("** class doesn't exist **\n", output)
